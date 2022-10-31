0157fae11db37ed78dc81b624b232d5a41058a74
Je <joimh@naver.com>
2022-10-31 17:12:29 +0900

    State문 테스트

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import logo from "./logo.svg";
 
 function App() {
     let post = "강남 우동 맛집22";
 
+    // state: 변동시 자동으로 html에 반영되게 만들고 싶을 때 작성
+    let [글제목, b] = useState([
+        "남자코트 추천",
+        "여자코트 추천",
+        "우동 맛집 추천",
+    ]);
+    let [내용, c] = useState(["2022년 10월 발행", "테스트 진행 중", "Error"]);
+
     return (
         <div className="App">
             <div className="black-nav">
                 <h4>블로그</h4>
             </div>
-            <h4 class="test" style={{ color: "red" }}>
-                {post}
-            </h4>
+            <div className="list">
+                <h4>{글제목[0]}</h4>
+                <p>{내용[0]}</p>
+            </div>
+            <div className="list">
+                <h4>{글제목[1]}</h4>
+                <p>{내용[1]}</p>
+            </div>
+            <div className="list">
+                <h4>{글제목[2]}</h4>
+                <p>{내용[2]}</p>
+            </div>
         </div>
     );
 }
