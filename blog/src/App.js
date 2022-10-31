356abba2c844b6dcef900ff823a80a4f131e6bfa
Je <joimh@naver.com>
2022-10-31 14:35:37 +0900

    eslintrc.json 설정

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let post = "강남 우동 맛집22";
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>블로그</h4>
+            </div>
+            <h4 style={{ color: "red" }}>{post}</h4>
+        </div>
+    );
 }
 
 export default App;
