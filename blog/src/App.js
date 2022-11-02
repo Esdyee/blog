3a3b652bcfb8bdcfb142dfb5d5e8be0cb09b82c0
Je <joimh@naver.com>
2022-11-02 15:39:17 +0900

    동적인 UI 테스트

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,13 +6,21 @@ function App() {
 	let post = "강남 우동 맛집22";
 
 	// state: 변동시 자동으로 html에 반영되게 만들고 싶을 때 작성
-	let [글제목, 글제목변경] = useState(["남자코트 추천", "여자코트 추천", "우동 맛집 추천"]);
+	let [글제목, 글제목변경] = useState(["남자코트 추천", "고객 추천", "우동 맛집 추천"]);
 	let [내용, c] = useState(["2022년 10월 발행", "테스트 진행 중", "Error"]);
 	let [좋아요, 좋아요변경] = useState(0);
+	let [modal, modal변경] = useState(false);
 
 	function 함수() {
 		// add count 좋아요
 		좋아요 = 좋아요 + 1;
+	}
+
+	function 정렬() {
+		let array = [...글제목];
+		array.sort();
+		console.log(array);
+		글제목변경(array);
 	}
 
 	return (
@@ -20,7 +28,13 @@ function App() {
 			<div className="black-nav">
 				<h4>블로그</h4>
 			</div>
-			<button>정렬</button>
+			<pre>{글제목}</pre>
+			<button
+				onClick={() => {
+					정렬();
+				}}>
+				정렬
+			</button>
 			<div className="list">
 				<h4>
 					{글제목[0]}{" "}
@@ -45,9 +59,26 @@ function App() {
 				<p>{내용[1]}</p>
 			</div>
 			<div className="list">
-				<h4>{글제목[2]}</h4>
+				<h4
+					onClick={() => {
+						modal변경(!modal);
+					}}>
+					{글제목[2]}
+				</h4>
 				<p>{내용[2]}</p>
 			</div>
+			{/*// Model condition html*/}
+			{modal === true ? <Modal></Modal> : null}
+		</div>
+	);
+}
+
+function Modal() {
+	return (
+		<div className="modal">
+			<h4>제목</h4>
+			<p>날짜</p>
+			<p>상세내용</p>
 		</div>
 	);
 }
